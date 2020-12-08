f2444af5c118ba508b57a67dac3532af667d6c28
Ovais Tariq <ovaistariq31@gmail.com>
2020-12-08 06:16:52 +0500

    V 1.1 Done

--- interviewTask.go
+++ interviewTask.go
@@ -3,12 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/csv"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+	"io"
+	"strconv"
 )
 
 const (
@@ -20,58 +19,69 @@ const (
 )
 
 func main() {
-	//Opening and creating DB
-	db := createAndOpenDB()
+	// var choice string
+	// fmt.Println("Welcome to the GoLang System\n Press 'Y' to execute anyother to exit\n")
+	// fmt.Printf("Choice:")
+	// fmt.Scanln(&choice)
 
-	// Open the file
-	csvfile, err := os.Open("persons.csv")
-	ErrorCheck(err, "CSV File Opened Successfully")
-	// Reading the file
-	readFile := csv.NewReader(csvfile)
+	// if choice == "Y" || choice == "y" {
+		//Opening and creating DB
+		db := createAndOpenDB()
 
-	//SQL Query
-	stmt, err := db.Prepare("INSERT INTO persons VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?)")
-	ErrorCheck(err, "No Errors in SQL Statement")
+		// Open the file
+		csvfile, err := os.Open("persons.csv")
+		ErrorCheck(err, "CSV File Opened Successfully")
+		// Reading the file
+		readFile := csv.NewReader(csvfile)
 
-	i := 0
-	success := 0
-	// Iterating through the records
-	for {
-		// Reading each record from readFile
-		record, err := readFile.Read()
-		if err == io.EOF {
-			//stop the loop when EOF reached
-			break
+		//SQL Query
+		stmt, err := db.Prepare("INSERT INTO persons VALUES (?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?)")
+		ErrorCheck(err, "No Errors in SQL Statement")
+
+		i := 0
+		success := 0
+		// Iterating through the records
+		for {
+			// Reading each record from readFile
+			record, err := readFile.Read()
+			if err == io.EOF {
+				//stop the loop when EOF reached
+				break
+			}
+			if err != nil {
+				//notify if any error comes during the process
+				ErrorCheck(err, "Unexpected error occurred!")
+				break
+			}
+			if i != 0 { //skipping the header
+				age, _ := strconv.Atoi(record[5])
+				emp, _ := strconv.ParseBool(record[17])
+				_, err = stmt.Exec(0, record[0], record[1], record[2], record[3], record[4], age, record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13], record[14], record[15], record[16], emp, record[18], record[19], record[20])
+				success++
+			} else {
+				i++
+			}
 		}
-		if err != nil {
-			//notify if any error comes during the process
-			ErrorCheck(err, "Unexpected error occurred!")
-			break
-		}
-		if i != 0 { //skipping the header
-			age, _ := strconv.Atoi(record[5])
-			emp, _ := strconv.ParseBool(record[17])
-			_, err = stmt.Exec(0, record[0], record[1], record[2], record[3], record[4], age, record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13], record[14], record[15], record[16], emp, record[18], record[19], record[20])
-			success++
+		if success > 0 {
+			log.Printf("Success! %d Records Inserted Into '%s' Table \n", success, tablename)
+
+			defer db.Close()
+			ReadInsertedRecords()
+
 		} else {
-			i++
+			log.Println("Oops!Something Weird Happened")
+			defer db.Close()
 		}
-	}
-	if success > 0 {
-		fmt.Printf("Success! Data Inserted Into '%s' Table \n", tablename)
-		defer db.Close()
-		ReadInsertedRecords()
-
-	} else {
-		fmt.Println("Oops!Something Weird Happened")
-		defer db.Close()
-	}
+	// }else {
+	// 	log.Println("Thank you for using the system.\nGoodBye!!")
+	// 	os.Exit(1)
+	// }
 }
 
 func createAndOpenDB() *sql.DB {
 
 	db, err := sql.Open("mysql", username+":"+password+"@tcp("+hostname+")/")
-	ErrorCheck(err, "DB Server Connection Successful")
+	ErrorCheck(err, "Connection String has no Error(s)")
 	PingDB(db)
 
 	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + dbname)
@@ -99,7 +109,7 @@ func ReadInsertedRecords() {
 	ErrorCheck(err, "Nothing wrong with Query")
 	defer rows.Close()
 	i:=0
-	fmt.Println("-------------------------------------------------------------------------------------------------------")
+	log.Println("-------------------------------------------------------------------------------------------------------")
 	for rows.Next() {
 		var id, age, zip int
 		var EmploymentStatus bool
@@ -112,16 +122,14 @@ func ReadInsertedRecords() {
 			&company, &designation, &YearlyRevenue)
 		ErrorCheck(err, "")
 
-		fmt.Printf("id: %d First Name: %s  Last Name: %s  SSN: %s  MotherTongue: %s  Race: %s  "+
+		log.Printf("id: %d First Name: %s  Last Name: %s  SSN: %s  MotherTongue: %s  Race: %s  "+
 			"Age: %d  BloodGroup: %s  Gender: %s  Birthday: %s  CcNumber: %s  Phone: %s  Email: %s  "+
 			"Street: %s  State: %s  City: %s  Zip: %d  GraduatedFrom: %s  EmploymentStatus: %t  "+
 			"Company: %s  Designation: %s  YearlyRevenue: %s \n\n", id, first, last, ssn, MotherTongue, race, age, BloodGroup, gender, birthday, CcNumber, phone, email, street, state, city, zip, GraduatedFrom, EmploymentStatus, company, designation, YearlyRevenue)
 		i++
 	}
-	fmt.Println("*******************************************************************************************************")
-	fmt.Printf("%d Records Inserted \n",i)
-
-
+	log.Println("*******************************************************************************************************")
+	log.Printf("%d Records Present in %s \n",i,tablename)
 	ErrorCheck(err, "Records Ended")
 	defer db.Close()
 
@@ -131,7 +139,7 @@ func ErrorCheck(err error, message string) {
 	if err != nil {
 		panic(err.Error())
 	} else {
-		fmt.Println(message)
+		log.Println(message)
 	}
 }
 
